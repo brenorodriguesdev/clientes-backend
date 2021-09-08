0b13fa7d560f4efe70c42ddbdff4870988ac5bee
brenorodriguesdev <brenorodriguesdev@gmail.com>
2021-09-08 20:02:34 -0300

    feat: add create client controller

---
+++ src/presentation/controllers/create-client.ts
@@ -0,0 +1,30 @@
+import { CreateClientUseCase } from '../../domain/usecases/create-client'
+import { Validator } from '../../validation/contracts/validator'
+import { Controller } from '../protocols/controller'
+import { HttpRequest, HttpResponse } from '../protocols/http'
+import { badRequest, created, serverError } from '../protocols/http-helper'
+
+export class CreateClientController implements Controller {
+  constructor (private readonly validator: Validator, private readonly createClientUseCase: CreateClientUseCase) { }
+  async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
+    try {
+      const error = this.validator.validate(httpRequest.body)
+      if (error) {
+        return badRequest(error)
+      }
+      const { nome, cpf, dataNascimento, rendaFamiliar } = httpRequest.body
+      const hasError = await this.createClientUseCase.create({
+        nome,
+        cpf,
+        dataNascimento,
+        rendaFamiliar
+      })
+      if (hasError) {
+        return badRequest(hasError)
+      }
+      return created()
+    } catch (error) {
+      return serverError()
+    }
+  }
+}
