8119cb298959bed10dcb54925953bc0db88b433a
brenorodriguesdev <brenorodriguesdev@gmail.com>
2021-09-10 22:45:10 -0300

    feat: add log decorator controller on create client controller

--- src/main/factories/controllers/create-client-controller-factory.ts
+++ src/main/factories/controllers/create-client-controller-factory.ts
@@ -1,10 +1,15 @@
 import { CreateClientService } from '../../../data/services/create-client/create-client'
+import { LogErrorElasticSearchRepository } from '../../../infra/repositories/elasticsearch/log-error-elasticsearch-repository'
 import { ClientMongoRepository } from '../../../infra/repositories/mongodb/client-mongo-repository'
 import { CreateClientController } from '../../../presentation/controllers/create-client'
+import { Controller } from '../../../presentation/protocols/controller'
+import { LogDecoratorController } from '../../decorators/log-decorator-controller'
 import { makeCreateClientValidation } from '../validations/create-client-validation-factory'
 
-export const makeCreateClientControler = (): CreateClientController => {
+export const makeCreateClientControler = (): Controller => {
   const clientMongoRepository = new ClientMongoRepository()
   const createClientService = new CreateClientService(clientMongoRepository, clientMongoRepository)
-  return new CreateClientController(makeCreateClientValidation(), createClientService)
+  const createClientController = new CreateClientController(makeCreateClientValidation(), createClientService)
+  const logErrorElasticSearchRepository = new LogErrorElasticSearchRepository()
+  return new LogDecoratorController(createClientController, logErrorElasticSearchRepository)
 }
