ef3c89f81b62eacc88cfc40a2b136b0437686dfd
brenorodriguesdev <brenorodriguesdev@gmail.com>
2021-09-11 14:23:50 -0300

    test: garantir que o handle seja chamado com o httpRequest correto

---
+++ src/main/decorators/log-decorator-controller.spec.ts
@@ -0,0 +1,58 @@
+import { LogErrorRepository } from '../../data/contracts/log-error-repository'
+import { Controller } from '../../presentation/protocols/controller'
+import { HttpRequest, HttpResponse } from '../../presentation/protocols/http'
+import { created } from '../../presentation/protocols/http-helper'
+import { LogDecoratorController } from './log-decorator-controller'
+
+const makeController = (): Controller => {
+  class ControllerStub implements Controller {
+    async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
+      return await new Promise(resolve => resolve(created()))
+    }
+  }
+  return new ControllerStub()
+}
+
+const makeLogErrorRepository = (): LogErrorRepository => {
+  class LogErrorRepositoryStub implements LogErrorRepository {
+    async logError (stack: string): Promise<void> {
+      return await new Promise(resolve => resolve())
+    }
+  }
+  return new LogErrorRepositoryStub()
+}
+
+export interface SutTypes {
+  controller: Controller
+  logErrorRepository: LogErrorRepository
+  sut: LogDecoratorController
+}
+
+const makeSut = (): SutTypes => {
+  const controller = makeController()
+  const logErrorRepository = makeLogErrorRepository()
+  const sut = new LogDecoratorController(controller, logErrorRepository)
+  return {
+    controller,
+    logErrorRepository,
+    sut
+  }
+}
+
+const httpRequest: HttpRequest = ({
+  body: {
+    nome: 'any_nome',
+    cpf: 'any_cpf',
+    dataNascimento: new Date('1999-03-23'),
+    rendaFamiliar: 2000
+  }
+})
+
+describe('Log Decorator Controller', () => {
+  test('Garantir que o handle seja chamado com o httpRequest correto', async () => {
+    const { sut, controller } = makeSut()
+    const handleSpy = jest.spyOn(controller, 'handle')
+    await sut.handle(httpRequest)
+    expect(handleSpy).toHaveBeenCalledWith(httpRequest)
+  })
+})
